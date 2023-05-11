042993bf854a4681db6309e77babfde430f879f9
Diego Batista Pereira <diegobpcelular@gmail.com>
2023-05-11 19:03:59 -0300

    feat: user repository & user service

---
+++ src/main/java/javaProject/SpringSecurity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package javaProject.SpringSecurity.repository;
+
+import javaProject.SpringSecurity.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/javaProject/SpringSecurity/service/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package javaProject.SpringSecurity.service.impl;
+
+import javaProject.SpringSecurity.repository.UserRepository;
+import javaProject.SpringSecurity.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
