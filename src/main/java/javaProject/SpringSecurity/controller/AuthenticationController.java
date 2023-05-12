cb24b2dc967e78df46e6182079367f34183c9e13
Diego Batista Pereira <diegobpcelular@gmail.com>
2023-05-11 21:29:02 -0300

    feat: authorization & authentication controllers added

---
+++ src/main/java/javaProject/SpringSecurity/controller/AuthenticationController.java
@@ -0,0 +1,30 @@
+package javaProject.SpringSecurity.controller;
+
+import javaProject.SpringSecurity.dao.request.SignInRequest;
+import javaProject.SpringSecurity.dao.request.SignUpRequest;
+import javaProject.SpringSecurity.dao.request.response.JwtAuthenticationResponse;
+import javaProject.SpringSecurity.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
